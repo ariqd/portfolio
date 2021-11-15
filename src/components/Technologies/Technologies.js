03e73a42cedff436550398bbd64103773cb58ec6
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-11-15 20:35:50 +0700

    tech section done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react"
+import { DiFirebase, DiReact, DiUikit } from "react-icons/di"
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents"
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles"
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
-);
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle main>Technologies</SectionTitle>
+    <SectionText>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis, ab minima?
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Experienced with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            Experienced with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiUikit size="3rem" />
+        <ListContainer>
+          <ListTitle>UI / UX</ListTitle>
+          <ListParagraph>
+            Experienced with <br />
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+)
 
-export default Technologies;
+export default Technologies
