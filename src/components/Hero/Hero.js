363925179e5df6d110d28b12645443899d69e880
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-11-15 20:07:34 +0700

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react"
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents"
+import Button from "../../styles/GlobalComponents/Button"
+import { LeftSection } from "./HeroStyles"
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam nam
+        labore atque voluptas officia velit nulla fuga ab quibusdam magni,
+        necessitatibus nobis delectus distinctio corrupti? Quidem excepturi
+        delectus quas veniam?
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero
